403d53bda145ffa742cbcc9aaec9c7139a7d0da0
Bruno <bruno.lorenzi.catunda@gmail.com>
2025-05-24 16:12:03 -0300

    fix: change app to accept cookie

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/router";
+import cookieParser = require("cookie-parser");
 
 dotenv.config();
 
 const app = express();
-
+app.use(cookieParser())
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
