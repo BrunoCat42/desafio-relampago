e40058f21887826facf7b8fdb60e66f797a57dc3
Bruno <bruno.lorenzi.catunda@gmail.com>
2025-05-28 18:39:17 -0300

    fix: correct navigation & cookie saved

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,8 +7,13 @@ import cookieParser = require("cookie-parser");
 dotenv.config();
 
 const app = express();
-app.use(cookieParser())
-app.use(cors());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/", router);
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
@@ -10,7 +10,6 @@ function App() {
   const { user } = useAuth();
   return (
     <AuthProvider>
-      <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
@@ -20,7 +19,6 @@ function App() {
             element={user ? <DashboardPage /> : <Navigate to="/login" />}
           />{" "}
         </Routes>
-      </BrowserRouter>
     </AuthProvider>
   );
 }
