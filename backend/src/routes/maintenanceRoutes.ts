61c2cbce54d74f665f3b39429a187ef94eea42c2
Bruno <bruno.lorenzi.catunda@gmail.com>
2025-05-30 00:46:59 -0300

    fix: add middleware on assets routes

--- backend/src/routes/maintenanceRoutes.ts
+++ backend/src/routes/maintenanceRoutes.ts
@@ -5,8 +5,11 @@ import {
   patchMaintenanceById,
   deleteMaintenanceById,
 } from "../controllers/maintenanceController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
+
+router.use(authMiddleware)
 
 router.post("/", postMaintenance);
 router.get("/", getAllMaintenances);
